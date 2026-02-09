babb1991363e2dcdc0dc9ec7c91a0dad7aacc501
Deepansu-Official <deepansu@joshtechnologygroup.com>
2026-02-09 18:35:26 +0530

    [A3-06] : Refactored movie list container

--- src/containers/MovieList/MovieList.container.tsx
+++ src/containers/MovieList/MovieList.container.tsx
@@ -1,9 +1,7 @@
 import { ComponentProps } from 'react';
 
-import { ArrowRight } from 'lucide-react';
-
-import { DetailCard, Glow, Link, Spinner, Typography } from '@components';
-import { ERRORS, ROUTE_PATH } from '@constants';
+import { DetailCard, Spinner, Typography } from '@components';
+import { ERRORS } from '@constants';
 import { useInfiniteScroll } from '@hooks';
 import { useMovieListInfiniteQuery } from '@services';
 import { formatDate, formatMinutesToTimeDelta } from '@utils';
@@ -31,64 +29,49 @@ export const MovieList = ({ filters }: MovieListContainerProps) => {
         isFetchingNextPage,
     });
 
+    if (isLoading) {
+        return (
+            <div className="flex justify-center">
+                <Spinner className="h-20 w-20" />
+            </div>
+        );
+    }
+
     if (isError) {
         throw new Error(ERRORS[500]);
     }
 
-    return (
-        <div className="flex flex-col mx-auto gap-5 p-5 relative xl:w-8/10">
-            <Glow className="w-20 h-20" />
-            <Glow className="bottom-0 right-0" />
-            <div className="flex justify-between flex-wrap">
-                <Typography tag="h1" variant="h1">
-                    Latest Movies
-                </Typography>
-                <Link to={ROUTE_PATH.MOVIES} variant="default">
-                    <Typography variant="span" tag="span">
-                        View All
-                    </Typography>
-                    <ArrowRight />
-                </Link>
-            </div>
-            {isLoading ? (
-                <div className="flex justify-center">
-                    <Spinner className="h-20 w-20" />
-                </div>
-            ) : (
-                movies?.length == 0 && (
-                    <Typography variant="h2" tag="h2">
-                        No Movies Found
-                    </Typography>
-                )
-            )}
-            <div className="flex flex-wrap justify-around xl:justify-between gap-20">
-                {movies?.map((movie, movieIndex) => {
-                    const imageConfig: ComponentProps<'img'> = {
-                        src: movie.image_url,
-                        alt: movie.name,
-                    };
-                    const isLastElement = movieIndex === movies.length - 1;
+    if (movies?.length == 0) {
+        return (
+            <Typography variant="h2" tag="h2">
+                No Movies Found
+            </Typography>
+        );
+    }
 
-                    return (
-                        <DetailCard
-                            ref={isLastElement ? lastElementRef : null}
-                            key={movie.id}
-                            title={movie.name}
-                            redirectTo={`${encodeURI(movie.name)}-${movie.id}`}
-                            mainImageProps={imageConfig}
-                            footer={{
-                                subtitle1: formatDate(
-                                    movie.release_date,
-                                    'DDMMYY',
-                                ),
-                                subtitle2: formatMinutesToTimeDelta(
-                                    movie.duration,
-                                ),
-                            }}
-                        />
-                    );
-                })}
-            </div>
+    return (
+        <div className="flex flex-wrap justify-around gap-20 w-fit">
+            {movies?.map((movie, movieIndex) => {
+                const imageConfig: ComponentProps<'img'> = {
+                    src: movie.image_url,
+                    alt: movie.name,
+                };
+                const isLastElement = movieIndex === movies.length - 1;
+
+                return (
+                    <DetailCard
+                        ref={isLastElement ? lastElementRef : null}
+                        key={movie.id}
+                        title={movie.name}
+                        redirectTo={`${encodeURI(movie.name)}-${movie.id}`}
+                        mainImageProps={imageConfig}
+                        footer={{
+                            subtitle1: formatDate(movie.release_date, 'DDMMYY'),
+                            subtitle2: formatMinutesToTimeDelta(movie.duration),
+                        }}
+                    />
+                );
+            })}
         </div>
     );
 };
--- src/pages/Home/Home.page.tsx
+++ src/pages/Home/Home.page.tsx
@@ -1,3 +1,7 @@
+import { ArrowRight } from 'lucide-react';
+
+import { Glow, Link, Typography } from '@components';
+import { ROUTE_PATH } from '@constants';
 import { MovieCarousel, MovieList } from '@containers';
 
 import { HOME_PAGE_MOVIE_FILTERS } from './Home.constant';
@@ -9,7 +13,22 @@ export default function HomePage() {
     return (
         <div className="font-inter scrollbar-stable">
             <MovieCarousel />
-            <MovieList filters={HOME_PAGE_MOVIE_FILTERS} />
+            <div className="flex flex-col mx-auto gap-5 py-5 sm:p-5 relative w-full xl:w-8/10">
+                <Glow className="w-20 h-20" />
+                <Glow className="bottom-0 right-0" />
+                <div className="flex justify-between flex-wrap">
+                    <Typography tag="h1" variant="h1">
+                        Latest Movies
+                    </Typography>
+                    <Link to={ROUTE_PATH.MOVIES} variant="default">
+                        <Typography variant="span" tag="span">
+                            View All
+                        </Typography>
+                        <ArrowRight />
+                    </Link>
+                </div>
+                <MovieList filters={HOME_PAGE_MOVIE_FILTERS} />
+            </div>
         </div>
     );
 }
